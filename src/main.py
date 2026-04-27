d78771497a9dc5729766ba8069d3630e5ff4d5f3
Mada <mariamada496@gmail.com>
2026-04-27 11:48:54 +0000

    implementa controle de led com botão

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
-print("Teste")
+from machine import Pin
+import time
+
+led = Pin(2, Pin.OUT)
+button = Pin(4, Pin.IN)
+
+while True:
+    if button.value() == 1:
+        led.on()
+    else:
+        led.off()
+    
+    time.sleep(0.1)
+    
